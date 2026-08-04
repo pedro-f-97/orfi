cc57e3c7e94a79dd685f99dc10337475f69f05b9
Pedro F. <pedroferreira250597@gmail.com>
2026-08-04 09:50:21 +0100

    feat: adicionar função de listar ficheiros encontrados

--- ficheiros.py
+++ ficheiros.py
@@ -1,3 +1,4 @@
+from os import path
 from pathlib import Path
 from shutil import copy2
 
@@ -13,12 +14,19 @@ def devolveExt(pasta: Path) -> set[str]:
     else:
         return ext
 
+def devolveFicheiros(pasta: Path) -> list[Path]:
+    listaFicheiros = list()
+    for ficheiro in pasta.iterdir():
+        if not ficheiro.is_dir(): #apenas ficheiros, não pastas
+            listaFicheiros.append(ficheiro)
+    return listaFicheiros
+
 def copiaFicheiros(ficheiro: Path, destino: Path):
     # 1º tem de preparar o caminho inteiro do ficheiro no destino, juntando destino / ficheiro, ex "C:\Users\pedroferreira\Documents\exemplo.jpg"
     ficheiroFinal = destino / ficheiro.name
     # 2º condição se, caso já exista um ficheiro com o mesmo nome no destino, pergunta se quer substituir o existente
     if ficheiroFinal.exists():
-        resposta = input(f"Já existe o ficheiro {ficheiro.name} na pasta {destino}, substituir? (s/n)")
+        resposta = input(f"Já existe o ficheiro {ficheiro.name} na pasta {destino}, substituir? (s/n): ")
         if resposta.lower() != "s":
             print("Cancelado")
             return
