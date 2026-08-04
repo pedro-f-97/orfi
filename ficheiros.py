2058c56dcbc6de423b71a80b2e1de6c7e386ac63
Pedro F. <pedroferreira250597@gmail.com>
2026-08-04 08:57:08 +0100

    feat: nova função para copiar ficheiros

--- ficheiros.py
+++ ficheiros.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from shutil import copy2
 
 
 def devolveExt(pasta: Path) -> set[str]:
@@ -11,3 +12,16 @@ def devolveExt(pasta: Path) -> set[str]:
         return ext
     else:
         return ext
+
+def copiaFicheiros(ficheiro: Path, destino: Path):
+    # 1º tem de preparar o caminho inteiro do ficheiro no destino, juntando destino / ficheiro, ex "C:\Users\pedroferreira\Documents\exemplo.jpg"
+    ficheiroFinal = destino / ficheiro.name
+    # 2º condição se, caso já exista um ficheiro com o mesmo nome no destino, pergunta se quer substituir o existente
+    if ficheiroFinal.exists():
+        resposta = input(f"Já existe o ficheiro {ficheiro.name} na pasta {destino}, substituir? (s/n)")
+        if resposta.lower() != "s":
+            print("Cancelado")
+            return
+    # 3º se resposta for sim ou condição se for falsa, copia o ficheiro
+    copy2(ficheiro, ficheiroFinal)
+    print(f"{ficheiro.name} copiado") 
