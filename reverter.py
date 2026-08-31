a2c1399eee340e4bcc61493232b03ed9ccca7461
Pedro F. <pedroferreira250597@gmail.com>
2026-08-31 17:20:20 +0100

    feat: adicionar funcionalidade de eliminar pastas vazias após reverter

--- reverter.py
+++ reverter.py
@@ -23,4 +23,10 @@ def reverte(pastaSelecionada: Path, categorias: list[configs.CategoriaDePasta], 
     total = 0
     for ficheiro in ficheirosParaReverter:
         total += trabalho(ficheiro, pastaSelecionada)
+
+    if tratamento == "movidos.":
+        for pasta in pastasParaReverter:
+            if not any(pasta.iterdir()):
+                print(f"{configs.CoresTexto.VERDE}Pasta vazia '{pasta}' foi eliminada.{configs.CoresTexto.RESET}")
+                pasta.rmdir()
     print(f"{configs.CoresTexto.VERDE}Revertido, {total} ficheiros {tratamento}{configs.CoresTexto.RESET}")
