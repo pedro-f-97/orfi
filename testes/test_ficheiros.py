69ef1f71f8efc9e91154d7b5cb07b6a148481a1f
Pedro F. <pedroferreira250597@gmail.com>
2026-08-31 11:53:10 +0100

    test: adicionar testes para moveFicheiro e apagaFicheiro

--- testes/test_ficheiros.py
+++ testes/test_ficheiros.py
@@ -2,11 +2,13 @@ from pathlib import Path
 
 from configs import CategoriaDePasta
 from ficheiros import (
+    apagaFicheiro,
     copiaFicheiro,
     defineDestino,
     devolveExt,
     devolveFicheiros,
     encontraCategoria,
+    moveFicheiro,
 )
 
 
@@ -82,3 +84,35 @@ def test_encontraCategoria(tmp_path):
 
     assert categoria is not None
     assert categoria.nome == "Notas"
+
+def test_moveFicheiro(tmp_path):
+    paraMover = (tmp_path / "texto.txt")
+    paraMover.touch()
+    pastaDestino = (tmp_path / "Destino")
+    pastaDestino.mkdir()
+    assert moveFicheiro(paraMover, pastaDestino) == 1
+
+    ficheiroMovido = Path(pastaDestino / "texto.txt")
+    assert paraMover.exists() == False
+    assert ficheiroMovido.exists()
+    assert ficheiroMovido.is_file()
+
+def test_existente_moveFicheiro(tmp_path, monkeypatch):
+    paraMover = (tmp_path / "texto.txt")
+    paraMover.touch()
+    pastaDestino = (tmp_path / "Destino")
+    pastaDestino.mkdir()
+    (pastaDestino / "texto.txt").touch()
+    monkeypatch.setattr("builtins.input", lambda _: "n")
+    assert moveFicheiro(paraMover, pastaDestino) == 0
+    assert paraMover.exists()
+    ficheiroMovido = Path(pastaDestino / "texto.txt")
+    assert ficheiroMovido.exists()
+    assert ficheiroMovido.is_file()
+
+def test_apagaFicheiro(tmp_path):
+    paraApagar = (tmp_path / "texto.txt")
+    paraApagar.touch()
+    assert paraApagar.exists()
+    assert apagaFicheiro(paraApagar) == 1
+    assert paraApagar.exists() == False
