00da719d131d2ea2eb9ea21e86376623afce7634
Pedro F. <pedroferreira250597@gmail.com>
2026-09-01 17:18:13 +0100

    feat: adicionar leitura e criação de configuração TOML

--- orfi/configs.py
+++ orfi/configs.py
@@ -1,16 +1,26 @@
 import os
 import sys
+import tomllib
 from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
 
 
+def caminhoConfiguracao() -> Path:
+    if sys.platform == "win32":
+        return Path(os.environ["APPDATA"]) / "orfi" / "config.toml"
+    else:
+        return Path.home() / ".config" / "orfi" / "config.toml"
 class CoresTexto:
     VERDE = "\033[92m"
     VERMELHO = "\033[91m"
     AMARELO = "\033[93m"
     AZUL = "\033[94m"
     RESET = "\033[0m"
+
+class Modo(Enum):
+    COPIAR = "copiar"
+    MOVER = "mover"
 
 
 @dataclass
@@ -29,15 +39,32 @@ def iniciarCategorias() -> list[CategoriaDePasta]:
     categorias.append(CategoriaDePasta("Outros", set(),None ,True))
     return categorias
 
-def lerCategorias():
-    print()
+def criarConfiguracaoStandard(caminho: Path):
+    caminho.parent.mkdir(parents=True, exist_ok=True)
 
-class Modo(Enum):
-    COPIAR = "copiar"
-    MOVER = "mover"
+    configuracaoStandard = Path(__file__).parent / "config.toml"
 
-def caminhoConfiguracao() -> Path:
-    if sys.platform == "win32":
-        return Path(os.environ["APPDATA"]) / "orfi" / "config.toml"
-    else:
-        return Path.home() / ".config" / "orfi" / "config.toml"
+    configuracao = configuracaoStandard.read_bytes()
+    caminho.write_bytes(configuracao)
+
+def carregarConfiguracao(caminho: Path | None = None) -> list[CategoriaDePasta]:
+    if caminho is None:
+        caminho = caminhoConfiguracao()
+
+    if not caminho.exists():
+        criarConfiguracaoStandard(caminho)
+
+    with caminho.open("rb") as ficheiro:
+        data = tomllib.load(ficheiro)
+
+    categorias = []
+
+    for categoria in data["categorias"]:
+        novaCategoria = CategoriaDePasta(
+            nome=categoria["nome"],
+            extensoes=set(categoria["extensoes"]),
+            defeito=categoria.get("defeito", False)
+        )
+        categorias.append(novaCategoria)
+
+    return categorias
