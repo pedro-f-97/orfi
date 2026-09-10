c0a2081538de70e35c6ddfeb3c83570c6f9c0e9e
Pedro F. <pedroferreira250597@gmail.com>
2026-09-10 09:07:45 +0100

    feat: adicionar resultados à função reverte

--- orfi/reverter.py
+++ orfi/reverter.py
@@ -5,7 +5,7 @@ from . import configs, ficheiros, mensagens, pastas
 
 logger = logging.getLogger(__name__)
 
-def reverte(pastaSelecionada: Path, categorias: list[configs.CategoriaDePasta], modo: configs.Modo, force: bool, simula: bool):
+def reverte(pastaSelecionada: Path, categorias: list[configs.CategoriaDePasta], modo: configs.Modo, force: bool, simula: bool) -> configs.ResultadosOperacao:
     """Reverte a organização dos ficheiros contidos nas pastas categorizadas dentro da pasta indicada.
 
     Args:
@@ -14,7 +14,13 @@ def reverte(pastaSelecionada: Path, categorias: list[configs.CategoriaDePasta], 
         modo: Define se os ficheiros são movidos ou copiados.
         force: Se aceita automaticamente todas as verificações ou não.
         simula: Se é para apenas simular o processo ou não.
+
+    Returns:
+        O resultado da operação, número de ficheiros tratados e
+        de pastas eliminadas (0 em ambos se nada foi feito).
     """
+    resultados = configs.ResultadosOperacao()
+
     if modo == configs.Modo.COPIAR:
         trabalho = ficheiros.copiaFicheiro
         tratamento = mensagens.mensagemTrataIdioma("tratamento_reverter_copia")
@@ -23,37 +29,38 @@ def reverte(pastaSelecionada: Path, categorias: list[configs.CategoriaDePasta], 
         tratamento = mensagens.mensagemTrataIdioma("tratamento_reverter_movimento")
     else:
         mensagens.mensagem("modo_inesperado", "modo_inesperado", False, mensagens.CoresTexto.VERMELHO, modo=modo)
-        return
+        return resultados
 
     pastasParaReverter = pastas.pastasExistentes(pastaSelecionada, categorias)
 
     if pastasParaReverter == set():
         mensagens.mensagem("nada_para_reverter", "nada_para_reverter_simula", simula, mensagens.CoresTexto.AMARELO)
-        return
+        return resultados
     
     ficheirosParaReverter = ficheiros.ficheirosParaReverter(pastasParaReverter)
 
     if ficheirosParaReverter == set():
         mensagens.mensagem("nada_para_reverter", "nada_para_reverter_simula", simula, mensagens.CoresTexto.AMARELO)
-        return
+        return resultados
     
-    total = 0
     ficheirosMovidos = set()
     for ficheiro in ficheirosParaReverter:
         resultado = trabalho(ficheiro, pastaSelecionada, force, simula)
         if resultado:
-            total += resultado
+            resultados.ficheirosTratados += resultado
             mensagens.mensagem("ficheiro_tratado", "ficheiro_seria_tratado", simula, mensagens.CoresTexto.AMARELO, ficheiro=ficheiro.name)
             if modo == configs.Modo.MOVER:
                 ficheirosMovidos.add(ficheiro)
 
     if modo == configs.Modo.MOVER:
-        pastasEliminadas = pastas.eliminaPastasVazias(pastasParaReverter, simula, ficheirosMovidos)
-        if pastasEliminadas > 0:
-            mensagens.mensagem("pastas_eliminadas", "pastas_eliminadas_simula", simula, mensagens.CoresTexto.VERMELHO, numero=pastasEliminadas)
+        resultados.pastasEliminadas = pastas.eliminaPastasVazias(pastasParaReverter, simula, ficheirosMovidos)
+        if resultados.pastasEliminadas > 0:
+            mensagens.mensagem("pastas_eliminadas", "pastas_eliminadas_simula", simula, mensagens.CoresTexto.VERMELHO, numero=resultados.pastasEliminadas)
     if not simula:
-        logger.info("Finished, %s files handled", total)
-    mensagens.mensagem("ficheiros_revertidos", "ficheiros_seriam_revertidos", simula, mensagens.CoresTexto.AMARELO, total=total, tratamento=tratamento)
+        logger.info("Finished, %s files handled", resultados.ficheirosTratados)
+    mensagens.mensagem("ficheiros_revertidos", "ficheiros_seriam_revertidos", simula, mensagens.CoresTexto.AMARELO, total=resultados.ficheirosTratados, tratamento=tratamento)
+
+    return resultados
 
 def reverteDatar(pastaSelecionada: Path, modo: configs.Modo, force: bool, simula: bool):
     """Remove o prefixo com data dos ficheiros contidos na pasta indicada.
