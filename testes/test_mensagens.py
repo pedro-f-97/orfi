24c90aa1d62b600582b6356abde3735eef43ed71
Pedro F. <pedroferreira250597@gmail.com>
2026-09-10 13:12:28 +0100

    test: adicionar teste para definirIdioma

--- testes/test_mensagens.py
+++ testes/test_mensagens.py
@@ -11,6 +11,16 @@ def test_definirIdioma():
     orfi.mensagens.definirIdioma(novoIdioma)
     assert novoIdioma == orfi.mensagens.idioma
 
+def test_definirIdiomaErro():
+    idiomaActual = orfi.mensagens.idioma
+    if idiomaActual == 'pt':
+        novoIdioma = 'en'
+    else:
+        novoIdioma = 'pt'
+    
+    assert orfi.mensagens.definirIdioma(novoIdioma)
+    assert novoIdioma == orfi.mensagens.idioma
+
 def test_mensagemTrataIdioma():
     orfi.mensagens.definirIdioma('pt')
     teste = orfi.mensagens.mensagemTrataIdioma('ficheiro_existente_substituir', ficheiro="teste", destino="teste")
